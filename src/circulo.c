c1e7f73e027099067e9b6c19867b434af9d7753a
Felilepe <felipesepulveda536@gmail.com>
2025-10-14 17:40:17 -0300

    Implemento resto do tad circulo

---
+++ src/circulo.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include "circulo.h"
+
+typedef struct circulo 
+{
+    int id;
+    double x, y, r;
+    char *corborda, *corpreench;
+
+}circulo;
+
+Circulo createCirculo(int id, double x, double y, double r, char *corborda, char*corpreench)
+{
+    circulo *circ= malloc(sizeof(circulo));
+    if(circ == NULL){
+        printf("Erro na alocação de memória");
+        exit(1);
+    }
+    
+    circ -> x = x;
+    circ -> y = y;
+    circ -> r = r;
+    
+    circ -> corborda = malloc(strlen(sizeof(corborda) + 1));
+    if(circ -> corborda == NULL){
+        printf("Erro na alocação da memório ao trocar cor da borda");
+        exit(1);
+    }
+
+    circ -> corpreench = malloc(strlen(sizeof(corpreench) + 1));
+    if(circ -> corpreench == NULL){
+        printf("Erro na alocação de memória ao trocar cor de preenchimento");
+        exit(1);
+    }
+
+}
+
