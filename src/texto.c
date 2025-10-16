e0f8b1072a3a5c9cc4fdc96d5b1c26183f05651f
Felilepe <felipesepulveda536@gmail.com>
2025-10-16 12:00:50 -0300

    Adicionado código para TAD texto

---
+++ src/texto.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "texto.h"
+
+typedef struct texto
+{
+    int id;
+    double x, y;
+    char *corb, *corp, *txto;
+    char a;
+}texto;
