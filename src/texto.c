71aadd6ef6fba8864f23b263d1225a51668aadee
Felilepe <felipesepulveda536@gmail.com>
2025-10-22 11:02:58 -0300

    Finalizado implementação TAD Texto

--- src/texto.c
+++ src/texto.c
@@ -7,9 +7,29 @@ typedef struct texto
 {
     int id;
     double x, y;
-    char *corb, *corp, *txto, *fFamily, *fWeight, *fSize;
-    char a;
+    char *corb, *corp, *txto, *fFamily, *fWeight, *fSize, *a;
 }texto;
+
+
+
+static char *default_fFamily = NULL;
+static char *default_fWeight = NULL;
+static char *default_fSize = NULL;
+
+static void inicializarDefault()
+{
+    free(default_fFamily);
+    default_fFamily = (char*)malloc(strlen("Arial") + 1);
+    strcpy(default_fFamily, "Arial");
+
+    free(default_fWeight);
+    default_fWeight = (char*)malloc(strlen("n") + 1);
+    strcpy(default_fWeight,"n");
+
+    free(default_fSize);
+    default_fSize = (char*)malloc(strlen("12") + 1);
+    strcpy(default_fSize, "12");
+}
 
 
 static double calcComp (char *txto) //Calcula o comprimento da linha
@@ -19,8 +39,13 @@ static double calcComp (char *txto) //Calcula o comprimento da linha
     
 
 
-Texto texto_create(int id, double x, double y, char *corb, char *corp, char a, char* txto)
+Texto texto_create(int id, double x, double y, char *corb, char *corp, char *a, char* txto)
 {
+    if(default_fFamily == NULL){
+        inicializarDefault;
+    }
+    
+
     texto *t = malloc(sizeof(texto));
     if(t == NULL){
         printf("Erro de alocacao de memoria ao criar texto");
@@ -34,26 +59,26 @@ Texto texto_create(int id, double x, double y, char *corb, char *corp, char a, c
     t -> a = a;
 
     
-    t -> fFamily = (char*)malloc(strlen("Arial") + 1);
+    t -> fFamily = (char*)malloc(strlen(default_fFamily) + 1);
     if(t -> fFamily == NULL){
     printf("Erro de alocacao de memoria ao atribuir fonte inicial do texto.");
     exit(1);
     }
-    strcpy(t -> fFamily, "Arial");
+    strcpy(t -> fFamily, default_fFamily);
     
-    t->fWeight = (char*)malloc(strlen("n") + 1);
+    t->fWeight = (char*)malloc(strlen(default_fWeight) + 1);
     if(t -> fFamily == NULL){
     printf("Erro de alocacao de memoria ao atribuir peso inicial do texto.");
     exit(1);
     }
-    strcpy(t->fWeight, "n");
+    strcpy(t->fWeight, default_fWeight);
 
-    t -> fSize = (char*)malloc(strlen("12") + 1);
+    t -> fSize = (char*)malloc(strlen(default_fSize) + 1);
     if(t -> fFamily == NULL){
     printf("Erro de alocacao de memoria ao atribuir tamanho inicial do texto.");
     exit(1);
     }
-    strcpy(t -> fSize, "12");
+    strcpy(t -> fSize, default_fSize);
     
     t -> corb = (char*)malloc((strlen(corb) + 1));
     if(t -> corb == NULL){
@@ -81,29 +106,6 @@ Texto texto_create(int id, double x, double y, char *corb, char *corp, char a, c
 }
 
 
-void texto_setStyle(Texto t, char *fFamily, char *fWeight, char* fSize)
-{
-    if(fFamily != NULL){
-        free(((texto*)t) -> fFamily);
-        ((texto*)t) -> fFamily = malloc(strlen(fFamily) + 1);
-        strcpy(((texto*)t) -> fFamily, fFamily);
-    } 
-    
-
-    if(fWeight != NULL){
-        free(((texto*)t) -> fWeight);
-        ((texto*)t) -> fWeight = malloc(strlen(fWeight) + 1);
-        strcpy(((texto*)t) -> fWeight, fWeight); 
-    } 
-    
-
-    if(fSize != NULL){
-        free(((texto*)t) -> fSize);
-        ((texto*)t) -> fSize = malloc(strlen(fSize) +1);
-        strcpy(((texto*)t) -> fSize, fSize);
-    }
-}
-
 
 int texto_getID(Texto t){return ((texto*)t) -> id;}
 
@@ -122,3 +124,82 @@ char *texto_getFamily(Texto t){return ((texto*)t) -> fFamily;}
 char *texto_getWeight(Texto t){return ((texto*)t) -> fWeight;}
 
 char *texto_getSize(Texto t){return ((texto*)t) -> fSize;}
+
+char *texto_getAnchor(Texto t){return ((texto*)t)  -> a;}
+
+double texto_getArea(Texto t){return 2 * calcComp(((texto*)t) -> txto);}
+
+
+
+void texto_setNewDefault(char *fFamily, char *fWeight, char* fSize)
+{
+    if(default_fFamily == NULL){
+        inicializarDefault;
+    }
+
+
+    free(default_fFamily);
+    default_fFamily = (char*)malloc(strlen(fFamily) + 1);
+    strcpy(default_fFamily, fFamily);
+
+    free(default_fWeight);
+    default_fWeight = (char*)malloc(strlen(fWeight) + 1);
+    strcpy(default_fWeight, fWeight);
+
+    free(default_fSize);
+    default_fSize = (char*)malloc(strlen(fSize) + 1);
+    strcpy(default_fSize, fSize);
+}
+
+void texto_setCoordX(Texto t, double x) {((texto*)t) -> x = x;}
+
+void texto_seCoordY(Texto t, double y) {((texto*)t) -> y = y;}
+
+void texto_setcorP(Texto t, char* corp)
+{
+    free(((texto*)t) -> corp);
+    ((texto*)t) -> corp = (char*)malloc(strlen(corp) + 1);
+    strcpy(((texto*)t) -> corp, corp);
+}
+
+void texto_setcorB(Texto t, char* corb)
+{
+    free(((texto*)t) -> corb);
+    ((texto*)t) -> corb = (char*)malloc(strlen(corb) + 1);
+    strcpy(((texto*)t) -> corb, corb);
+}
+
+void texto_setTexto(Texto t, char* txto)
+{
+    free(((texto*)t) -> txto);
+    ((texto*)t) -> txto = (char*)malloc(strlen(txto) + 1);
+    strcpy(((texto*)t) -> txto, txto);
+}
+
+void texto_setFamily(Texto t, char* fFamily)
+{
+    free(((texto*)t) -> fFamily);
+    ((texto*)t) -> fFamily = (char*)malloc(strlen(fFamily) + 1);
+    strcpy(((texto*)t) -> fFamily, fFamily);
+}
+
+void texto_setWeight(Texto t, char* fWeight)
+{
+    free(((texto*)t) -> fWeight);
+    ((texto*)t) -> fWeight = (char*)malloc(strlen(fWeight) + 1);
+    strcpy(((texto*)t) -> fWeight, fWeight);
+}
+
+void texto_setSize(Texto t, char* fSize)
+{
+    free(((texto*)t) -> fSize);
+    ((texto*)t) -> fSize = (char*)malloc(strlen(fSize) + 1);
+    strcpy(((texto*)t) -> fSize, fSize);
+}
+
+void texto_setAnchor(Texto t, char *a)
+{
+    free(((texto*)t) -> a);
+    ((texto*)t) -> a = (char*)malloc(strlen(a) + 1);
+    strcpy(((texto*)t) -> a, a);
+}
--- src/texto.h
+++ src/texto.h
@@ -3,7 +3,8 @@
 
 typedef void* Texto;
 
-Texto texto_create(int id, double x, double y, char *corb, char *corp, char a, char* txto);
+Texto texto_create(int id, double x, double y, char *corb, char *corp, char *a, char* txto);
+
 
 int texto_getID(Texto t);
 
@@ -13,6 +14,40 @@ double texto_getCoordy(Texto t);
 
 char *texto_getTexto(Texto t);
 
-Texto texto_setStyle(Texto t, char* fFamily, char* fWeight, char* fSize);
+char *texto_getAnchor(Texto t);
+
+double texto_getArea(Texto t);
+
+char *texto_getCorB(Texto t);
+
+char *texto_getCorP(Texto t);
+
+char *texto_getFamily(Texto t);
+
+char *texto_getWeight(Texto t);
+
+char *texto_getSize(Texto t);
+
+
+void texto_setNewDefault(char* fFamily, char* fWeight, char* fSize);
+
+void texto_setCoordX(Texto t, double x);
+
+void texto_seCoordY(Texto t, double y); 
+
+void texto_setcorP(Texto t, char* corp);
+
+void texto_setcorB(Texto t, char* corb);
+
+void texto_setTexto(Texto t, char* txto);
+
+void texto_setFamily(Texto t, char* fFamily);
+
+void texto_setWeight(Texto t, char* fWeight);
+
+void texto_setSize(Texto t, char* fSize);
+
+void texto_setAnchor(Texto t, char *a);
+
 
 #endif
