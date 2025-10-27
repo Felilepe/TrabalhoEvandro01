2bfe3f36bfc367d46fbaf8db0e98b8f1333dc147
Felilepe <felipesepulveda536@gmail.com>
2025-10-27 02:46:08 -0300

    Feat: Adicionadas as funções de shift e attachCarregador ao TAD de disparador

--- src/disparador.c
+++ src/disparador.c
@@ -1,8 +1,63 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "pilha.h"
+#include "disparador.h"
 
 struct disparador
 {
-        
+    Pilha *cesq, *cdir;
+    item pos_lanc;
+    double x, y;
+    int l;
+};
+
+Disparador *disparador_create(int l, double x, double y) //pd
+{
+    Disparador *d = (Disparador*)malloc(sizeof(Disparador));
+    if(d == NULL){
+        printf("Erro na alocacao de memoria ao criar disparador.");
+        exit(1);
+    }
+
+    d -> l = l;
+    d -> x = x;
+    d -> y = y;
+
+    d -> cdir = NULL;
+    d -> cesq = NULL;
+    d ->pos_lanc = NULL;
+
+    return d;
+}
+
+void disparador_attachCarregador(Disparador *d, Pilha *cesq, Pilha *cdir)
+{
+    d -> cesq = cesq;
+    d -> cdir = cdir;
+
+}
+
+item disparador_shift(Disparador *d, char b, int n)
+{
+    switch (b){
+    case 'e': 
+        for(int i = 0; i < n; i++){
+            if(d -> pos_lanc != NULL){
+                pilha_push(d -> cdir, d -> pos_lanc);
+            }
+            d -> pos_lanc = pilha_pop(d -> cesq);
+        } break;
+    case 'd':
+        for(int i = 0; i < n; i++){
+            if(d -> pos_lanc != NULL){
+                pilha_push(d -> cesq, d ->pos_lanc);
+            }
+            d -> pos_lanc = pilha_pop(d -> cdir);
+        } break;
+    
+    default: printf("Erro: Botao %c nao e uma opcao aceita.", b);
+    exit(1);
+
+
+    return d -> pos_lanc;
+    }
 }
--- src/disparador.h
+++ src/disparador.h
@@ -11,11 +11,11 @@ typedef void* item;
 
 Disparador *disparador_create();
 
-Carregador carregador_load(Carregador *c, int n);
-
 void disparador_attachCarregador(Disparador *d, Carregador *cesq, Carregador *cdir);
 
 item disparador_shift(Disparador *d, char b, int n);
+
+void disparador_destroy(Disparador *d);
 
 
 
