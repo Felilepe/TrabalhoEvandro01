5b73fd696216e562346b804a4400b5598046a327
Felilepe <felipesepulveda536@gmail.com>
2025-11-04 05:48:49 -0300

    Doc: Finalizada a documentação dos TADs

--- src/lerGeo.h
+++ src/lerGeo.h
@@ -5,7 +5,11 @@
 
 typedef Fila Chao;
 
-
+/******************************************************************
+*@brief Processa um arquivo .geo e carrega as formas no Chão.
+*@param nome_path_geo O caminho completo para o arquivo .geo.
+*@return Um ponteiro para o Chão (Fila*) contendo todas as formas lidas.
+******************************************************************/
 Chao *processaGeo(const char *nome_path_geo);
 
 #endif 
--- src/pilha.h
+++ src/pilha.h
@@ -6,54 +6,54 @@
 typedef void *item;
 typedef struct pilha Pilha;
 
-Pilha *pilha_create(); 
 /****************************************************
-*@brief cria uma pilha com tamanho = 0 e item = NULL*
-*@return um pointeiro para a pilha inicializada     *
+*@brief cria uma pilha com tamanho = 0 e item = NULL
+*@return um pointeiro para a pilha inicializada     
 ****************************************************/
+Pilha *pilha_create(); 
 
-bool pilha_isEmpty(Pilha *p); 
 /******************************************************************** 
- *@brief Verifica se a pilha está vazia                              *
- *@param Pilha Ponteiro para pilha que será checada                  *
- *@return True se a pilha estiver vazia, caso contrário retorna False*
+ *@brief Verifica se a pilha está vazia                              
+ *@param p Ponteiro para pilha que será checada                  
+ *@return True se a pilha estiver vazia, caso contrário retorna False
  ********************************************************************/
+bool pilha_isEmpty(Pilha *p); 
 
-void pilha_push(Pilha *p, item i); 
 /*****************************************************************
- *@brief Adiciona item ao topo da pilha                           *
- *@param Pilha Pointeiro para a pilha aonde será adicionado o item*
- *@param item Pointeiro para o item que será adicionado à pilha   *
+ *@brief Adiciona item ao topo da pilha                           
+ *@param Pilha Pointeiro para a pilha aonde será adicionado o item
+ *@param i Pointeiro para o item que será adicionado à pilha   
  *****************************************************************/
+void pilha_push(Pilha *p, item i); 
 
- item pilha_pop(Pilha *p); 
 /**********************************************************************************
-*@brief Remove o item no topo da pilha, retorna um erro caso a pilha esteja vazia.*
-*@param p Pointeiro para a pilha                                                  *
-*@return Retorna o endereço para o item                                           *              
+*@brief Remove o item no topo da pilha, retorna um erro caso a pilha esteja vazia.
+*@param p Pointeiro para a pilha                                                  
+*@return Retorna o endereço para o item                                                      
 **********************************************************************************/
+ item pilha_pop(Pilha *p); 
 
+/******************************************************************
+*@brief Mostra o item no topo da pilha                            
+*@param p Pointeiro para a pilha que será checada                 
+*@return Retorna o ponteiro para o item que está no topo da pilha.
+*Retorna um erro caso a pilha esteja vaiza*                       
+******************************************************************/
 item pilha_peek(Pilha *p); 
-/******************************************************************
-*@brief Mostra o item no topo da pilha                            *
-*@param p Pointeiro para a pilha que será checada                 *
-*@return Retorna o ponteiro para o item que está no topo da pilha.*
-*Retorna um erro caso a pilha esteja vaiza*                       *
-******************************************************************/
 
+/******************************************************************
+*@brief Pega o número de itens atualmente na pilha.               
+*@param p Ponteiro para a pilha.                                  
+*@return O número de itens (int) na pilha.                        
+******************************************************************/
 int pilha_getSize(Pilha *p);
-/******************************************************************
-*@brief Pega o número de itens atualmente na pilha.               *
-*@param p Ponteiro para a pilha.                                  *
-*@return O número de itens (int) na pilha.                        *
-******************************************************************/
 
-void pilha_destroy(Pilha *p); 
 /******************************************************************
-*@brief Libera toda a memória alocada pela pilha (nós e estrutura)*
-*@param p Ponteiro para a pilha a ser destruída                   *
-*@return Não há retorno                                           *
+*@brief Libera toda a memória alocada pela pilha (nós e estrutura)
+*@param p Ponteiro para a pilha a ser destruída                   
+*@return Não há retorno                                           
 ******************************************************************/
+void pilha_destroy(Pilha *p); 
 
 
 #endif
